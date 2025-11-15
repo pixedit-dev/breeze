b74cb1316f29ae61678eb2f140b6e544f41cd9f6
christmas <pixedit.2023@gmail.com>
2025-11-15 23:18:30 +0330

    fix: change context path issue for deployment

--- context/ThemeProvider.jsx
+++ context/ThemeProvider.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { ThemeContext } from "./themeContext";
+import { ThemeContext } from "./ThemeContext";
 
 export const ThemeProvider = ({ children }) => {
 	const [theme, setTheme] = useState("light");
